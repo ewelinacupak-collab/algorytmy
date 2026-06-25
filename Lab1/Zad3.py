a26cc4ab65028b267c8c28204f95ca9fba5b9ada
ewelinacupak-collab <ewelinacupak@gmail.com>
2026-06-25 11:49:54 +0200

    Update Zad3.py

--- Lab1/Zad3.py
+++ Lab1/Zad3.py
@@ -1,16 +1,18 @@
 tablica = [1, 5, 7, 8, 3, 20, 7]
 szukana = int(input("Jakiej liczby szukamy?"))
 
-i = 0
-N = len(tablica)
-znaleziono = False
+i = 0 #zaczynamy od pierwszego elementu
+N = len(tablica) #długość tablicy
+znaleziono = False #domyślnie zakłądamy, że liczby brak
 
+#przechodzimy przez all tablicę
 while i < N:
     if tablica[i] == szukana:
-        znaleziono = True
-        break
-    i += 1
+        znaleziono = True #jak element pod indexem jest równy szukanej zmieniamy wartość
+        break #przerywamy
+    i += 1 #do kolejnego indexu
 
+#sprawdzamy stan i piszemy
 if znaleziono:
     print("Znaleziono liczbę")
 else:
