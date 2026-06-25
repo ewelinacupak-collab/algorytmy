b9304fb4e01cfd89afd21238f19898cf06d6aeed
ewelinacupak-collab <ewelinacupak@gmail.com>
2026-06-25 11:51:39 +0200

    Update Zad4.py

--- Lab1/Zad4.py
+++ Lab1/Zad4.py
@@ -1,13 +1,15 @@
 tablica = [4, 20, 6, 8, 4, 2]
 N = len(tablica)
 
-min = tablica[0]
-i = 1
+min = tablica[0] #zakładamy, że first element is the lowest
+i = 1 #start from second element
 
+#przeszukujemy
 while i < N:
+    #jak element przeszukiwany is lower than nasz element minimalny
     if tablica[i] < min:
-        min = tablica[i]
+        min = tablica[i] #aktualizujemy go
 
-    i += 1
+    i += 1 #idziemy next
 
 print(min)
