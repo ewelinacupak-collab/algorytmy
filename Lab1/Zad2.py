02490f1e22697866aa0765bc01a7b081536d62d1
ewelinacupak-collab <ewelinacupak@gmail.com>
2026-06-25 11:47:38 +0200

    Update Zad2.py

--- Lab1/Zad2.py
+++ Lab1/Zad2.py
@@ -1,12 +1,18 @@
+#info ile liczb sprawdzić
 N = int(input("Podaj ilość liczb, które chcesz sprawdzić"))
-ile_ujemnych = 0
-i = 0
+ile_ujemnych = 0 #pom, będziemy tu sumować
+i = 0 # zmienna sterująca pętlą, licznik obrotów
 
+#pętla wykona się N razy
 while i < N:
+    #w każdym obrocie prosimy o liczbę
     liczba = float(input("Podaj liczbę"))
 
+    #check czy liczba większa od zera
     if liczba < 0:
+        #tak, zwiększamy licznik
         ile_ujemnych += 1
 
-    i += 1
+    i += 1 #zwiąkszamy i, żeby nie była nieskończona
+#po zakończeniu wypisujemy
 print(" Liczba ujemnych to", ile_ujemnych)
